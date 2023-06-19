fea3f8b400c456b9baed4f423c90962c0978766f
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-06-19 14:43:49 +0100

    MyNvim

---
+++ nvim/lua/josean/plugins/lualine.lua
@@ -0,0 +1,7 @@
+-- import lualine plugin safely
+local status, lualine = pcall(require, "lualine")
+if not status then
+  return
+end
+
+lualine.setup()
---
+++ nvim/lua/josean/plugins/nvim-tree.lua
@@ -0,0 +1,11 @@
+-- import nvim-tree plugin safely
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+  return
+end
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded_netrw = true
+vim.g.loaded_netrwPlugin = true
+
+nvimtree.setup()
